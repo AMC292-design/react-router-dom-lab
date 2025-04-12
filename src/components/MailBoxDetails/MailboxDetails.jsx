067b180c3e36852dba77fd0d255c182348000921
Ashley Corbett <ashleycorbett@Ashleys-MacBook-Pro.local>
2025-04-12 01:44:10 -0400

    I have an error in the browser console; I beleive something is wrong with my code too. However, turining in what I do have

--- src/components/MailBoxDetails/MailboxDetails.jsx
+++ src/components/MailBoxDetails/MailboxDetails.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 const MailboxDetails = ({ mailboxes }) =>
 {
   const { mailboxId } = useParams();
-  const mailbox = mailboxes.find((box) => box._id === Number(mailboxId));
+  const mailbox = mailboxes.find((box) => box._id === mailboxId);
   if (!mailbox)
   {
     return <h1>Mailbox Not Found!</h1>;
--- src/components/NewLetter/NewLetter.jsx
+++ src/components/NewLetter/NewLetter.jsx
@@ -1,10 +1,10 @@
 const NewLetter = () => {
-  return (
-    <div>
-      <h1>New Letter</h1>
-      <p>This is where the letter-sending form will go.</p>
-    </div>
-  );
-};
-
-export default NewLetter;
+    return (
+      <div>
+        <h1>New Letter</h1>
+        <p>This is where the letter-sending form will go.</p>
+      </div>
+    );
+  };
+  
+  export default NewLetter;
